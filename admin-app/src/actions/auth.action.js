87444c6eb8e9d6842ab637fe79787d11929f5dd6
jungbum <jungbumwoo@naver.com>
2021-02-27 15:01:03 +0900

    Signout Api

--- admin-app/src/actions/auth.action.js
+++ admin-app/src/actions/auth.action.js
@@ -51,9 +51,20 @@ export const isUserLoggedIn = () => {
 
 export const signout = () => {
     return async dispatch => {
-        localStorage.clear();
         dispatch({
             type: authConstants.LOGOUT_REQUEST
         })
+        const res = await axiosInstance.post(`/admin/signout`);
+        if(res.status === 200) {
+            localStorage.clear();
+            dispatch({
+                type: authConstants.LOGOUT_SUCCESS
+            });
+        } else {
+            dispatch({
+                type: authConstants.LOGOUT_FAILURE,
+                payload: { error: res.data.error }
+            })
+        }
     }
 }
