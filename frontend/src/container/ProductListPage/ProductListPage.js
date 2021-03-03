97835ce187fde257ca330754f6f231b8ca86a424
jungbumwoo <jungbumwoo@naver.com>
2021-03-03 12:06:35 +0900

    fetch product api and render product list(2)

--- frontend/src/container/ProductListPage/ProductListPage.js
+++ frontend/src/container/ProductListPage/ProductListPage.js
@@ -12,7 +12,27 @@ const ProductListPage = (props) => {
 
     return(
         <Layout>
-            <p>Product List Page</p>
+            <div className="card">
+                <div className="cardHeader">
+                    <div>Samsung mobile under 10k</div>
+                    <button>View All</button>
+                </div>
+                <div>
+                    <div className="productContainer">
+                        <div className="productImgContainer">
+                            <img src="http://localhost:2000/public/WZpPQoK5I-52e27980185045.Y3JvcCwxOTk5LDE1NjQsMCwzOQ.png" alt=""/>
+                        </div>
+                        <div>
+                            <div>Samsumg 4gb phone</div>
+                            <div>
+                                <span>4.3</span>
+                                <span>3353</span>
+                            </div>
+                            <div>5000</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Layout>
     )
 };
